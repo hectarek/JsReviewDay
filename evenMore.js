cfb8afefb2609eeab6ce8da09c3f2cdb7fa7eaab
Hector Gonzalez <hectarek13@gmail.com>
2020-04-08 15:22:33 -0400

    almost done

--- evenMore.js
+++ evenMore.js
@@ -62,17 +62,17 @@
 var array7 = [1,2,3,4,5,6,7,8,9,10,11,12];
 var array7a = [];
 
-// console.log(array7.join(' '));
-
-for (var i=1; i <= array7.length; i++){
-
-    for (var x=1; x <= 12; x++){
-        var result = array7[x] * i;
-        array7a.push(result);
+function isPrime(val){
+    for (var i=2; i < val; i++){
+        if (val % i === 0) {
+            return false
+        }
     }
-
-    // console.log(array7a.join(' '))
+    return value > 1
 }
+
+
+console.log(isPrime(8))
 
 // 8. Write a program that prints all prime numbers. (Note: if your programming
 // language does not support arbitrary size numbers, printing all primes up to the
@@ -186,47 +186,183 @@ var top = 100000000
 
 // console.log(sum);
 
-// 16.Write a function that tests whether a string is a palindrome.
+// // 16.Write a function that tests whether a string is a palindrome.
 
-function palindromeFinder(word) {
+// function palindromeFinder(word) {
 
-    var wordRev = word.reverse(); 
+//     //split up each letter into an array
+//     var splitStr = word.split('');
+//     // reverse that new array to flip the word
+//     var reverseStr = splitStr.reverse()
+//     // join it back into a word that is now backwards
+//     var joinStr = reverseStr.join('');
 
-    if(word == wordRev){
-        return "Yep, that's a palindrome"
-    } else {
-        return "No this is not a palindrome"
-    }
+//     var wordRev = joinStr; 
 
-}
+//     if(word == wordRev){
+//         return "Yep, that's a palindrome"
+//     } else {
+//         return "No this is not a palindrome"
+//     }
 
-console.log(palindromeFinder('this'));
+// }
 
-// 17.Write three functions that compute the sum of the numbers in a list: using a
-// for-loop, a while-loop and recursion. (Subject to availability of these constructs in
-// your language of choice.)
-// 18.Write a function on_all that applies a function to every element of a list. Use it to
-// print the first twenty perfect squares. The perfect squares can be found by
-// multiplying each natural number with itself. The first few perfect squares are 1*1=
-// 1, 2*2=4, 3*3=9, 4*4=16. Twelve for example is not a perfect square because
-// there is no natural number m so that m*m=12. (This question is tricky if your
-// programming language makes it difficult to pass functions as arguments.)
-// 19.Write a function that concatenates two lists. [a,b,c], [1,2,3] → [a,b,c,1,2,3]
-// 20.Write a function that combines two lists by alternatingly taking elements, e.g.
-// [a,b,c], [1,2,3] → [a,1,b,2,c,3].
-// 21.Write a function that merges two sorted lists into a new sorted list. [1,4,6],[2,3,5]
-// → [1,2,3,4,5,6]. You can do this quicker than concatenating them followed by a
-// sort.
-// 22.Write a function that rotates a list by k elements. For example [1,2,3,4,5,6]
-// rotated by two becomes [3,4,5,6,1,2]. Try solving this without creating a copy of
-// the list. How many swap or move operations do you need?
+// console.log(palindromeFinder('tacocat'));
+
+// // 17.Write three functions that compute the sum of the numbers in a list: using a
+// // for-loop, a while-loop and recursion. (Subject to availability of these constructs in
+// // your language of choice.)
+
+// var array17 = [1, 34, 54, 29, 14, 84, 65, 39, 52, 64, 77, 93, 3, 13, 61, 10]
+// var sum = 0
+
+// for (let i=0; i < array17.length; i++){
+//     sum += array17[i]
+
+// }
+// console.log(sum);
+
+// var sum2 = 0;
+// var c = 0;
+
+// while (c < array17.length){
+//     sum2 += array17[c]
+//     c++;
+// }
+
+// console.log(sum2);
+
+// var sum3 = 0;
+
+
+
+
+// // 18.Write a function on_all that applies a function to every element of a list. Use it to
+// // print the first twenty perfect squares. The perfect squares can be found by
+// // multiplying each natural number with itself. The first few perfect squares are 1*1=
+// // 1, 2*2=4, 3*3=9, 4*4=16. Twelve for example is not a perfect square because
+// // there is no natural number m so that m*m=12. (This question is tricky if your
+// // programming language makes it difficult to pass functions as arguments.)
+// var array18 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+// function on_all(num){
+//     return num * num
+// }
+
+// for(let i=0; i < array18.length; i++){
+//     array18[i] = on_all(array18[i]);
+// }
+
+// console.log(array18)
+
+
+// // 19.Write a function that concatenates two lists. [a,b,c], [1,2,3] → [a,b,c,1,2,3]
+// var array19a = ['a','b','c']
+// var array19b = [1,2,3]
+// var newArray = array19a.concat(array19b);
+
+// console.log(newArray);
+
+
+// // 20.Write a function that combines two lists by alternatingly taking elements, e.g.
+// // [a,b,c], [1,2,3] → [a,1,b,2,c,3].
+// var array20a = ['a','b','c']
+// var array20b = [1,2,3]
+// var newArray = []
+
+// for (let i=0; i < array20a.length; i++){
+//     newArray.push(array20a[i])
+//     newArray.push(array20b[i])
+// }
+
+// console.log(newArray)
+
+// // 21.Write a function that merges two sorted lists into a new sorted list. [1,4,6],[2,3,5]
+// // → [1,2,3,4,5,6]. You can do this quicker than concatenating them followed by a sort.
+// var array21a = [1,4,6] 
+// var array21b = [2,3,5]
+
+// var newArray = array21a.concat(array21b).sort()
+
+// console.log(newArray)
+
+
+// // 22.Write a function that rotates a list by k elements. For example [1,2,3,4,5,6]
+// // rotated by two becomes [3,4,5,6,1,2]. Try solving this without creating a copy of
+// // the list. How many swap or move operations do you need?
+// var array22 = [1,2,3,4,5,6];
+
+// function rotate(num){
+
+//     for(let i=0; i < num; i++){
+//         array22.push(array22.shift())
+//     }
+
+//     return array22
+
+// }
+
+// console.log(rotate(4));
+
+
 // 23.Write a function that computes the list of the first 100 Fibonacci numbers. The
-// first two Fibonacci numbers are 1 and 1. The n+1-st Fibonacci number can be
-// computed by adding the n-th and the n-1-th Fibonacci number. The first few are
-// therefore 1, 1, 1+1=2, 1+2=3, 2+3=5, 3+5=8.
-// 24.Write a function that takes a number and returns a list of its digits. So for 2342 it
-// should return [2,3,4,2].
-// 25.Write a function that translates a text to Pig Latin and back. English is translated
-// to Pig Latin by taking the first letter of every word, moving it to the end of the
-// word and adding ‘ay’. “The quick brown fox” becomes “Hetay uickqay rownbay
-// oxfay”
+// // first two Fibonacci numbers are 1 and 1. The n+1-st Fibonacci number can be
+// // computed by adding the n-th and the n-1-th Fibonacci number. The first few are
+// // therefore 1, 1, 1+1=2, 1+2=3, 2+3=5, 3+5=8.
+
+// function fibin(){
+//     var fib = 1
+//     var prev = 0
+//     var temp = 0
+//     var output = []
+
+//     for (let i=0; i < 100; i++){
+//         temp = fib
+//         fib = fib + prev
+//         prev = temp
+//         output.push(prev)
+//     }
+
+//     return output
+// }
+
+// console.log(fibin());
+
+
+// // 24.Write a function that takes a number and returns a list of its digits. So for 2342 it
+// // should return [2,3,4,2].
+// var num = 2342
+// var splitNum = num.toString().split('')
+// var myList = splitNum.map(function(i){return parseInt(i)})
+
+// console.log(myList);
+
+
+// // 25.Write a function that translates a text to Pig Latin and back. English is translated
+// // to Pig Latin by taking the first letter of every word, moving it to the end of the
+// // word and adding ‘ay’. “The quick brown fox” becomes “Hetay uickqay rownbay
+// // oxfay”
+
+// var mySentence = 'Did you do the laundry'
+
+// function pigLatin(sen){
+
+//    var array25 = sen.split(' ')
+
+//    var newAr = array25.map(function(i){
+//     var disect = i.split('');
+//     disect.push( (disect.shift() + "ay") )
+
+//     var joinAr = disect.join('')
+
+//     console.log (joinAr)  
+//     return joinAr
+
+//    })
+
+//    return newAr.join(' ')
+
+// }
+
+// console.log(pigLatin(mySentence));
+
