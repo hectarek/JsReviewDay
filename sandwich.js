162163d2fa905cc8a03adab59fb00ea793e5dc47
Hector Gonzalez <hectarek13@gmail.com>
2020-04-08 08:36:50 -0400

    first commit

---
+++ sandwich.js
@@ -0,0 +1,12 @@
+function sandwichCalc(slicesOfBread, slicesOfCheese) {
+   
+    var sandwiches = Math.floor(slicesOfBread / 2)
+    
+    if 
+
+    console.log(sandwiches)
+
+    return sandwiches
+}
+
+sandwichCalc(11);
